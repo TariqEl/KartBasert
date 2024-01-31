e80baf658d35e1ef3258404330e26852de1067b3
TariqEl <123506602+TariqEl@users.noreply.github.com>
2024-01-31 16:30:12 +0100

    Added a somewhat map

--- Lecture1/vite-project/src/App.tsx
+++ Lecture1/vite-project/src/App.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import { MutableRefObject, useEffect, useRef } from 'react';
 import "./App.css"
+import { Map, View } from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from "ol/source/OSM";
+import { useGeographic } from 'ol/proj';
+
+useGeographic();
+
+const map = new Map({
+  layers: [
+    new TileLayer({source: new OSM()})
+  ],
+  view: new View({
+    center: [10, 59], zoom: 8
+  })
+});
+
 
 const App = () => {
+
+  const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
+
+  useEffect(()=>{
+    map.setTarget(mapRef.current)
+  },[]);
+
   return (
-    <div>
-      Hello world
+    <div className='map' ref={mapRef}>
+      I am a map
     </div>
   );
 };
