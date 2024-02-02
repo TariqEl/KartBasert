1fdfe5bf9be5d7488015cea58e9c40d0d3acadc3
TariqEl <123506602+TariqEl@users.noreply.github.com>
2024-02-02 20:00:33 +0100

    A place for the map to be

---
+++ src/components/MapApplication.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import "./application.css";
+
+export function MapApplication(){
+    return <>
+        <header><h1>Map</h1></header>
+        <nav>Actions</nav>
+        <main>Here is the map</main>
+    </>
+}
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { MapApplication } from "./components/MapApplication";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello</h1>)
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+
+
+root.render(<MapApplication/>)
