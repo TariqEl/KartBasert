5981b51380ab9e756a814003918111e5e6553acb
TariqEl <123506602+TariqEl@users.noreply.github.com>
2024-02-08 23:51:26 +0100

    Add lecture 3 stuff

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Application } from "./modules/application/Application";
+import { MapApplication } from "./components/MapApplication";
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(<Application />);
+root.render(<MapApplication />);
