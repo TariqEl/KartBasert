6de99832c22c9cb785d76b709268c25284961748
Tariq Morabeti <tariqmorabeti@Tariqs-iMac.local>
2024-02-11 18:23:00 +0100

    Cleared files

--- src/main.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { MapApplication } from "./components/MapApplication";
-const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(<MapApplication />);
